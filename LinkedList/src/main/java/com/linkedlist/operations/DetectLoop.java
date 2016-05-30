c2da49ad5a1702e73819b7780bc3e16a1310d69c
Dheeraj Goyal <dheeraj@raremile.com>
2016-05-30 14:13:30 +0530

    An algorithm to remove the loop in a list after detection by floyd cycle detection algorithm

--- LinkedList/src/main/java/com/linkedlist/operations/DetectLoop.java
+++ LinkedList/src/main/java/com/linkedlist/operations/DetectLoop.java
@@ -27,6 +27,14 @@ public class DetectLoop
         System.out.println("Detected loop using floyd cycle detection: " + floydCycleFinding( listWithoutLoop.getHead() ));
         System.out.println("list with loop:");
         System.out.println("Detected loop using floyd cycle detection: " + floydCycleFinding( listWithLoop.getHead() ));
+        System.out.println("list without loop:");listWithoutLoop.printList();
+        System.out.println("Trying to remove loop if exist:" );
+        listWithoutLoop.setHead( removeLoop( listWithoutLoop.getHead() ) );
+        listWithoutLoop.printList();
+        System.out.println("list with loop :");
+        System.out.println("Trying to remove loop if exist:" );
+        listWithLoop.setHead( removeLoop( listWithLoop.getHead() ) );
+        listWithLoop.printList();
     }
 
 
@@ -88,6 +96,45 @@ public class DetectLoop
         return loop;
     }
 
-    public static Node RemoveLoop(Node head) throws Exception{
+
+    /**
+     * This uses Floyd Cycle Detection Algotrithm to detect loop. If there is loop acording to algorithm slow and fast pointer meets at some point
+     * within loop. Now in order to find the point where  the loop starts we start one pointer from head of list and run the slow pointer and head pointer
+     * ond by one until they meet again. The point of meeting is starting of the loop. We make the previous of slow pointer's next as null to terminate
+     * the loop.
+     * @param head head of the list
+     * @return new head of the list after removing loop
+     * @throws Exception
+     */
+    public static Node removeLoop(Node head){
+        if(head == null){
+            return null;
+        }
+        Node singleJump = head;
+        Node doubleJump = head;
+        boolean loop = false;
+        while(singleJump != null && doubleJump != null && doubleJump.getNext() != null){
+            singleJump = singleJump.getNext();
+            doubleJump = doubleJump.getNext().getNext();
+            if(singleJump == doubleJump){
+                loop = true;
+                break;
+            }
+        }
+        if(!loop){
+            return head;
+        }
+
+        Node meetingPoint = singleJump;
+        Node tempNode = head;
+        Node prev = null;
+        while(tempNode != meetingPoint){
+            prev = meetingPoint;
+            tempNode = singleJump.getNext();
+            meetingPoint = meetingPoint.getNext();
+        }
+
+        prev.setNext( null );
+        return head;
     }
 }
